307b51746e93a322ed8801d83a56d6e0d0febee1
Parth Sinha <sinhaparth555@gmail.com>
2024-12-15 15:19:39 +0000

    added more

--- main.c
+++ main.c
@@ -7,6 +7,9 @@ typedef enum {
     INST_PUSH,
     INST_POP,
     INST_ADD,
+    INST_SUB,
+    INST_MUL,
+    INST_DIV,
     INST_PRINT,
 } Inst_Set;
 
@@ -16,7 +19,9 @@ typedef struct {
 } Inst;
 
 Inst program[] = {
-    {.type = INST_PUSH, .value = 15},
+    {.type = INST_PUSH, .value = 13},
+    {.type = INST_PUSH, .value = 11},
+    {.type = INST_MUL},
     {.type = INST_PRINT},
 };
 #define PROGRAM_SIZE (sizeof(program)/sizeof(program[0]))
@@ -26,22 +31,51 @@ int stack[MAX_STACK_SIZE];
 int stack_size;
 
 void push(int value) {
+    if (stack_size >= MAX_STACK_SIZE) {
+        fprintf(stderr, "ERROR: Stack Overflow\n");
+        exit(1);
+    }
     stack[stack_size] = value;
     stack_size++;
 }
 
 int pop() {
+    if(stack_size <= 0) {
+        fprintf(stderr, "ERROR: Stack Underflow\n");
+        exit(1);
+    }
     return stack[--stack_size];
 }
 
 int main() {
+    int a, b;
     for(size_t ip = 0; ip < PROGRAM_SIZE; ip++) {
         switch(program[ip].type) {
             case INST_PUSH:
                 push(program[ip].value);
+                break;
             case INST_POP:
+                pop();
                 break;
             case INST_ADD:
+                a = pop();
+                b = pop();
+                push(a + b); 
+                break;
+            case INST_SUB:
+                a = pop();
+                b = pop();
+                push(a - b);
+                break;
+            case INST_MUL:
+                a = pop();
+                b = pop();
+                push(a * b);
+                break;
+            case INST_DIV:
+                a = pop();
+                b = pop();
+                push(b / a);
                 break;
             case INST_PRINT:
                 printf("%d\n", pop());
